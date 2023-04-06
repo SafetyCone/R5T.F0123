2ac28d0a6f5e551b808c9cca533339851ee2e3ab
davidcoats <david@getsafetycone.com>
2023-04-06 11:12:47 -0700

    Initial commit.

---
+++ source/R5T.F0123.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0123.Construction
+Construction console project for the R5T.F0123 library.
---
+++ source/R5T.F0123/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0123
+{
+	/// <summary>
+	/// Operations on instance descriptors.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
