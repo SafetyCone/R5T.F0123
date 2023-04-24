5f132ae52091bc7b24fd36235de7414fce8f9b38
davidcoats <david@getsafetycone.com>
2023-04-24 00:20:29 -0700

    Interim changes.

---
+++ source/R5T.F0123/Code/Functionality/IOperations.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using R5T.T0132;
+using R5T.T0161;
+using R5T.T0161.Extensions;
+using R5T.T0170;
+using R5T.T0172;
+
+
+namespace R5T.F0123
+{
+    [FunctionalityMarker]
+    public partial interface IOperations : IFunctionalityMarker
+    {
+        public ((
+            string Key,
+            SimpleTypeName SimpleTypeName,
+            NamespacedTypeName NamespacedTypeName,
+            IProjectFilePath ProjectFilePath) ContainingTypeInformation,
+            (InstanceDescriptor Instance,
+            SimplePropertyName SimplePropertyName,
+            OutputTypeName OutputTypeName)[] InstancesInformation)[]
+        Group_PropertiesAndDeclaringType(IEnumerable<InstanceDescriptor> instances)
+        {
+            var groups = instances
+                .Select(instance =>
+                {
+                    var kindMarkedFullPropertyName = instance.KindMarkedFullMemberName.AsKindMarkedFullPropertyName();
+
+                    var (simpleTypeName, namespacedTypeName, namespacedTypedPropertyName, fullPropertyName)
+                        = Instances.MemberNameOperator.Get_SimpleTypeName(kindMarkedFullPropertyName);
+
+                    var simplePropertyName = Instances.MemberNameOperator.Get_SimplePropertyName(
+                        namespacedTypedPropertyName);
+
+                    var outputType = Instances.MemberNameOperator.Get_OutputTypeName(fullPropertyName);
+
+                    return (instance, simpleTypeName, namespacedTypeName, simplePropertyName, outputType);
+                })
+                .GroupBy(x =>
+                {
+                    var key = $"{x.instance.ProjectFilePath}:{x.namespacedTypeName}";
+                    return key;
+                })
+                .Select(group =>
+                {
+                    // There will always be a first, since group must have a member to become a group in the group-by operator.
+                    var (instance, simpleTypeName, namespacedTypeName, _, _) = group.First();
+
+                    return (
+                        (
+                            group.Key,
+                            simpleTypeName,
+                            namespacedTypeName,
+                            instance.ProjectFilePath),
+                        group.Select(x =>
+                        {
+                            return (
+                                x.instance,
+                                x.simplePropertyName,
+                                x.outputType);
+                        })
+                        .ToArray());
+                })
+                .ToArray()
+                ;
+
+            return groups;
+        }
+    }
+}
--- source/R5T.F0123/Code/Instances.cs
+++ source/R5T.F0123/Code/Instances.cs
@@ -5,6 +5,6 @@ namespace R5T.F0123
 {
     public static class Instances
     {
-		
+        public static F0121.IMemberNameOperator MemberNameOperator => F0121.MemberNameOperator.Instance;
     }
 }
