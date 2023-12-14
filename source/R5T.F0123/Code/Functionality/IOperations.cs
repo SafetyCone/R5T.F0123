e1cd74426608701f9949a64edb9b4898094d350e
davidcoats <david@getsafetycone.com>
2023-12-14 01:33:36 -0800

    Interim changes.

--- source/R5T.F0123/Code/Functionality/IOperations.cs
+++ source/R5T.F0123/Code/Functionality/IOperations.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using R5T.L0065.T000;
 using R5T.T0132;
 using R5T.T0161;
 using R5T.T0161.Extensions;
@@ -14,6 +15,122 @@ namespace R5T.F0123
     [FunctionalityMarker]
     public partial interface IOperations : IFunctionalityMarker
     {
+        public IEnumerable<Signature> Enumerate_Signatures(IEnumerable<InstanceDescriptor> instances)
+        {
+            var output = instances
+                .Select(instance =>
+                {
+                    var signature = Instances.SignatureStringOperator.Get_Signature(instance.SignatureString);
+                    return signature;
+                });
+
+            return output;
+        }
+
+        /// <summary>
+        /// Gets signature instances from the signature string values of the input instances.
+        /// The instance descriptor is included in output tuples to allow associating the signature with the extra information the instance descriptor contains.
+        /// (There is no guarantee of uniqueness across the data values of the instance descriptor, so a dictionary mapping is out.)
+        /// </summary>
+        public IEnumerable<(InstanceDescriptor Instance, Signature Signature)> Enumerate_SignaturesAndInstances(IEnumerable<InstanceDescriptor> instances)
+        {
+            var output = instances
+                .Select(instance =>
+                {
+                    var signature = Instances.SignatureStringOperator.Get_Signature(instance.SignatureString);
+
+                    return (instance, signature);
+                });
+
+            return output;
+        }
+
+        public IEnumerable<(InstanceDescriptor Instance, TSignature Signature)> Enumerate_SignaturesAndInstancesAs<TSignature>(IEnumerable<InstanceDescriptor> instances)
+            where TSignature : class
+        {
+            var output = instances
+                .Select(instance =>
+                {
+                    var signature = Instances.SignatureStringOperator.Get_Signature(instance.SignatureString);
+
+                    var signatureAsType = signature as TSignature;
+
+                    return (instance, signatureAsType);
+                });
+
+            return output;
+        }
+
+        public IEnumerable<TSignature> Enumerate_SignaturesCastTo<TSignature>(IEnumerable<InstanceDescriptor> instances)
+            where TSignature : Signature
+        {
+            var output = this.Enumerate_Signatures(instances)
+                .Cast<TSignature>()
+                ;
+
+            return output;
+        }
+
+        public Signature[] Get_Signatures(IEnumerable<InstanceDescriptor> instances)
+        {
+            var output = this.Enumerate_Signatures(instances)
+                .Now();
+
+            return output;
+        }
+
+        public (InstanceDescriptor, Signature)[] Get_SignaturesAndInstances(IEnumerable<InstanceDescriptor> instances)
+        {
+            var output = this.Enumerate_SignaturesAndInstances(instances)
+                .Now();
+
+            return output;
+        }
+
+        public TSignature[] Get_SignaturesCastedTo<TSignature>(IEnumerable<InstanceDescriptor> instances)
+            where TSignature : Signature
+        {
+            var output = this.Get_SignaturesCastedTo<TSignature>(instances)
+                .Now();
+
+            return output;
+        }
+
+        public IEnumerable<IGrouping<TypeSignature, THasDeclaringType>> Group_ByDeclaringType<THasDeclaringType>(IEnumerable<THasDeclaringType> propertySignatures)
+            where THasDeclaringType : IHasDeclaringType
+        {
+            var output = propertySignatures.GroupBy(
+                x => x.DeclaringType,
+                IdentityBasedTypeSignatureEqualityComparer.Instance);
+
+            return output;
+        }
+
+        public IEnumerable<IGrouping<TypeSignature, T>> Group_ByDeclaringType<T>(
+            IEnumerable<T> items,
+            Func<T, TypeSignature> declaringTypeSelector)
+        {
+            var output = items.GroupBy(
+                declaringTypeSelector,
+                IdentityBasedTypeSignatureEqualityComparer.Instance);
+
+            return output;
+        }
+
+        public IEnumerable<IGrouping<TypeSignature, (InstanceDescriptor Instance, T Signature)>> Group_ByDeclaringType<T>(
+            IEnumerable<(InstanceDescriptor Instance, T Signature)> pairs)
+            where T : IHasDeclaringType
+        {
+            var output = this.Group_ByDeclaringType(
+               pairs,
+               x => x.Signature.DeclaringType);
+
+            return output;
+        }
+
+        /// <summary>
+        /// Property name-output type tuples are grouped by {Project File Path}:{Property Declaring Type Namespaced Type Name}.
+        /// </summary>
         public ((
             string Key,
             ISimpleTypeName SimpleTypeName,
--- source/R5T.F0123/Code/Instances.cs
+++ source/R5T.F0123/Code/Instances.cs
@@ -6,5 +6,6 @@ namespace R5T.F0123
     public static class Instances
     {
         public static F0121.IMemberNameOperator MemberNameOperator => F0121.MemberNameOperator.Instance;
+        public static L0065.F004.ISignatureStringOperator SignatureStringOperator => L0065.F004.SignatureStringOperator.Instance;
     }
 }
